857834e0950fa8e6273b0b32c1f2a85e9ed2e884
noloMRP <165516222+noloMRP@users.noreply.github.com>
2025-04-29 20:00:08 +0200

    Added a responsive Navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,38 +1,62 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
-import '/src/Components/Header/header.css'
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import "/src/Components/Header/header.css";
 
-
-export default function Header(){
-    
-    return(
-        <header>
-            
-            <nav>
-                <div className="logo-container">
-                    <img src="src\images\Icons\logo.svg" alt="logo" />
-                </div>
-                    
-                    {/* Use Link to create custom routes using react-router-dom(simpler than using switches for different links) */}                <ul>
-                    <CustomLink to="/main">MAIN</CustomLink>
-                    <CustomLink to="/projects">PROJECTS</CustomLink>
-                    <CustomLink to="/certifications">CERTIFICATONS</CustomLink>
-                    <CustomLink to="/contacts">CONTACTS</CustomLink>
-                    
-                </ul>
-            </nav>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
 // logic for active nav link
 // Matches current path to reletive one and assigns "active" to pathname
-function CustomLink({to, children, ...props}) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({path: resolvedPath.pathname, end: true})
-    return(
-        <li className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>
-            {children}
-            </Link>
-        </li>
-    )
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
 }
