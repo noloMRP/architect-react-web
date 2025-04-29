0e19e14e53f46850cfe27254a69b6cf827fbd6ee
Jayson Baloyi <jysnbly7@gmail.com>
2025-04-29 19:49:47 +0200

    Updated HTML for Landing Page

--- src/Components/Landing-Page/Main.jsx
+++ src/Components/Landing-Page/Main.jsx
@@ -2,7 +2,8 @@ import '/src/Components/Landing-Page/main.css'
 
 export default function Main(){
     return (
-        <section >
+        <>
+            <section >
             <div className="page">
                 <h1>PROJECT</h1>
                 <h2>Lorem</h2>
@@ -12,5 +13,55 @@ export default function Main(){
                 <button>VIEW PROJECT &#x2192; </button>
             </div>
         </section>
+        <section className='about-sect'>
+            <div className="about-container">
+                <div className="about-images">
+                    <img src="src\images\Landing-Page\about-page1.png" alt="" />
+                    <img src="src\images\Landing-Page\about-page2.png" alt="" />
+                    <img src="src\images\Landing-Page\about-page3.png" alt="" />
+                </div>
+                <div className="about-content">
+                    <h1>About Us</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium obcaecati accusamus quod iusto dolor! Minima amet ad in! Maxime eius eos eveniet unde natus laborum, omnis dolor repudiandae in ex!</p>
+                    <a href="#"> READ MORE &#x2192;</a>
+                </div>
+            </div>
+        </section>
+        <section className='mission-statement'>
+                    <h1>Main Focus/Mission Statement</h1>
+                    <div className="statement">
+                    <h1>1</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium obcaecati accusamus quod iusto dolor! Minima amet ad in! Maxime eius eos eveniet unde natus laborum, omnis dolor repudiandae in ex!</p>  
+                </div>
+        </section>
+        <section className='projects'>
+                    <h1>Our Projects</h1>
+                    {/* Duplicate project card div for more projects */}
+                    <div className="project-card">
+                    <img src="" alt="" />
+                    <h1>Sample Project</h1>
+                    <a href="/">VIEW MORE</a>
+                </div>
+        </section>
+        <section className='contact-us'>
+                    <h1>Contact Us</h1>
+                    <div className="form">
+                        <form action="">
+                            
+                            <input type="text" />
+                            
+                            <input type="text" />
+                            
+                            <input type="text" />
+                            
+                            <input type="text" />
+                            
+                            <input type="text" />
+                        
+                            </form>
+                    </div>
+        </section>
+        </>
+        
     )
 }
